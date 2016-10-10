148b0fd43b83fc21dd243a3431613468a9d4a712
appletonbrian <appleton@bu.edu>
2016-10-10 18:04:29 -0400

    Add files via upload

---
+++ mixed_sin_fft.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#sampling frequency, 1/seconds
+Fs = 1000
+#remember that the the nyquist frequency is Fs/2
+
+#number of samples
+N  = 512
+
+#give a frequency (Hz) for TWO superimposed signals for which we'll compute the FFT
+f1  = 100
+f2  = 300
+
+#calculate that signal's period (s)
+T1 = 1/f1
+T2 = 1/f2
+
+#impose a phase offset, radians
+phi = np.pi/3
+
+#create time array
+#total length of signal in time is N*sampling period.
+t = np.arange(0,N/Fs,1/Fs)
+
+#create frequency axis for FFT
+#fft bins go from 0 to Fs/N, in intervals of Fs/N
+freq = np.arange(0,Fs,Fs/N)
+
+#Create a simple sine signal on which to compute the FFT
+signal = np.sin(2*np.pi*t/T1+phi) + np.sin(2*np.pi*t/T2+phi)
+
+
+#Plot the signal vs. time
+fig1 = plt.figure(1)
+plt.plot(t, signal)
+plt.xlabel('Time, s')
+plt.ylabel('Amplitude')
+plt.suptitle('Signal in time domain')
+fig1.show()
+
+#compute the FFT, returns an array of complex numbers
+signal_fft = np.fft.fft(signal)
+
+#get the magnitude
+signal_fft_abs = np.absolute(signal_fft)
+
+#plot the magnitude vs. frequency
+fig2 = plt.figure(2)
+plt.plot(freq[0:int(N/2)], signal_fft_abs[0:int(N/2)])
+plt.xlabel('Frequency, Hz')
+plt.ylabel('Magnitude')
+plt.suptitle('Signal in frequency domain')
+fig2.show()
+
+#pause to keep figures alive
+print("Press enter to close figures")
+input()
+
+
+
+
+
