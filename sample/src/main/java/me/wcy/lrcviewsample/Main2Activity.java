da9a44bbb156354b21937b3220c0b70d2007f51c
apple <apple@dhcp-wifi-8021x-155-41-72-53.bu.edu>
2016-11-09 16:51:46 -0500

    first upload

---
+++ sample/src/main/java/me/wcy/lrcviewsample/Main2Activity.java
@@ -0,0 +1,114 @@
+package me.wcy.lrcviewsample;
+
+        import android.content.Intent;
+        import android.content.res.AssetManager;
+        import android.media.MediaPlayer;
+        import android.os.Bundle;
+        import android.os.Handler;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.widget.Button;
+
+        import java.io.IOException;
+        import java.io.InputStream;
+
+        import me.wcy.lrcview.LrcView;
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnCompletionListener {
+    private LrcView lrcBig;
+    //private LrcView lrcSmall;
+    private Button btnPlayPause;
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        final Button btn_back = (Button) findViewById(R.id.btn_back);
+        ButtonListenerButton3 buttonListenerButton3 = new ButtonListenerButton3();
+        btn_back.setOnClickListener(buttonListenerButton3);
+
+        lrcBig = (LrcView) findViewById(R.id.lrc_big);
+        //lrcSmall = (LrcView) findViewById(R.id.lrc_small);
+        btnPlayPause = (Button) findViewById(R.id.btn_play_pause);
+        btnPlayPause.setOnClickListener(this);
+        mediaPlayer.setOnCompletionListener(this);
+
+        AssetManager am = getAssets();
+        try {
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(am.openFd("cbg.mp3").getFileDescriptor());
+            mediaPlayer.prepare();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        lrcBig.loadLrc(getLrcText("rtl.lrc"));
+        //lrcSmall.loadLrc(getLrcText("cbg.lrc"));
+    }
+
+    private String getLrcText(String fileName) {
+        String lrcText = null;
+        try {
+            InputStream is = getAssets().open(fileName);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            lrcText = new String(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lrcText;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+            handler.post(runnable);
+        } else {
+            mediaPlayer.pause();
+            handler.removeCallbacks(runnable);
+        }
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+        lrcBig.onDrag(0);
+        //lrcSmall.onDrag(0);
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if (mediaPlayer.isPlaying()) {
+                long time = mediaPlayer.getCurrentPosition();
+                lrcBig.updateTime(time);
+                //lrcSmall.updateTime(time);
+            }
+
+            handler.postDelayed(this, 100);
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        handler.removeCallbacks(runnable);
+        mediaPlayer.reset();
+        mediaPlayer.release();
+        mediaPlayer = null;
+        super.onDestroy();
+    }
+
+    class ButtonListenerButton3 implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+            startActivity(intent);
+            mediaPlayer.stop();
+        }
+    }
+}
---
+++ sample/src/main/java/me/wcy/lrcviewsample/MainActivity.java
@@ -0,0 +1,34 @@
+package me.wcy.lrcviewsample;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import me.wcy.lrcview.LrcView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button2 = (Button) findViewById(R.id.button2);
+        ButtonListenerButton2 buttonListenerButton2 = new ButtonListenerButton2();
+        button2.setOnClickListener(buttonListenerButton2);
+    }
+    class ButtonListenerButton2 implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(intent);
+        }
+    }
+}
